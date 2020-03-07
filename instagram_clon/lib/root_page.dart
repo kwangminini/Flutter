5836f67d5f01c1bb776217e0335d665f5b7bcd69
kwangminini <rhkdals7362@gmail.com>
2020-03-08 00:03:52 +0900

    Instagram Clone Project

---
+++ instagram_clon/lib/root_page.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart'; //Android Design Material
+import 'package:instagramclon/tab_page.dart';
+
+class RootPage extends StatelessWidget { //페이지가 변하지 않는 페이지 이므로 StatelessWidget
+  @override
+  Widget build(BuildContext context) {
+    return TabPage();
+  }
+}
