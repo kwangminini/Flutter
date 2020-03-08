4026d71cea12d1d079d6590fa14ccef442abaf18
kwangminini <rhkdals7362@gmail.com>
2020-03-09 00:59:29 +0900

    BottomNavigation tab

--- instagram_clon/lib/tab_page.dart
+++ instagram_clon/lib/tab_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:instagramclon/home_page.dart';
 
 class TabPage extends StatefulWidget {
   @override
@@ -8,7 +9,7 @@ class TabPage extends StatefulWidget {
 class _TabPageState extends State<TabPage> {
   int _selectedIndex=0;
   List _pages=[
-    Text('page1'),
+    HomePage(),
     Text('page2'),
     Text('page3')
   ];
@@ -17,6 +18,7 @@ class _TabPageState extends State<TabPage> {
     return Scaffold(
       body: Center(child: _pages[_selectedIndex]),
       bottomNavigationBar: BottomNavigationBar(
+        fixedColor: Colors.black,
         onTap: _onItemTapped,
           currentIndex: _selectedIndex,
           items: <BottomNavigationBarItem>[
