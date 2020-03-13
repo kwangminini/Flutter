8302faf36e3f0c23bdb96f277b1668367a1060bb
kwangminini <rhkdals7362@gmail.com>
2020-03-14 03:14:10 +0900

    getPicture from album

--- instagram_clon/lib/create_page.dart
+++ instagram_clon/lib/create_page.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class CreatePage extends StatefulWidget {
   @override
@@ -8,6 +11,7 @@ class CreatePage extends StatefulWidget {
 class _CreatePageState extends State<CreatePage> {
   final textEditingController = TextEditingController();
 
+  File _image;
   @override
   void dispose() { //class가 종료될때 호출
     // TODO: implement dispose
@@ -20,7 +24,7 @@ class _CreatePageState extends State<CreatePage> {
       appBar: _buildAppBar(),
       body: _buildBody(),
       floatingActionButton: FloatingActionButton(
-        onPressed: null,
+        onPressed: _getImage,
         child: Icon(Icons.add_a_photo),
       ),
     );
@@ -35,13 +39,23 @@ class _CreatePageState extends State<CreatePage> {
   }
 
   Widget _buildBody() {
-    return Column(
+    return SingleChildScrollView( //자판이 올라오면서 사진의 영역을 침범하는것을 막기위해 사진쪽에 스크롤 뷰를 만들어준다. SingleChildScrollView
+        child: Column(
       children: <Widget>[
-        Text('No Image'),
+        _image == null ? Text('No Image') : Image.file(_image),
         TextField(controller: textEditingController,
           decoration: InputDecoration(hintText: '내용을 입력하세요'),
         )
       ],
-    );
+    ));
+  }
+
+  Future<void> _getImage() async {
+    var image = await ImagePicker.pickImage(source: ImageSource.gallery); //비동기로 나중에 콜백 , await 을 통해 비동기식으로 만들어 준다!!
+//    Future<File> image = ImagePicker.pickImage(source: ImageSource.gallery); //비동기로 나중에 콜백
+//    File image = await ImagePicker.pickImage(source: ImageSource.gallery); //비동기로 나중에 콜백
+    setState(() {
+      _image=image;
+    });
   }
 }
